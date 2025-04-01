79306e9df57bab9447f9b2ff143e43342dabc9fe
Victor <wortsac01@gmail.com>
2025-03-31 21:33:40 -0300

    finalizando o switch com js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.getElementById("file-input").addEventListener("change", function(event) {
   let fileName = event.target.files.length > 0 ? event.target.files[0].name : "Nenhum arquivo selecionado"; document.getElementById("file-name").innerHTML = "image.png";
 });
+
+var checkbox = document.getElementById("switch");
+var text = document.getElementById("switch-text");
+
+checkbox.addEventListener("change", function() {
+  text.textContent = this.checked ? "Claro" : "Escuro";
+});
