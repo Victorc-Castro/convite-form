2203c81adfacf8afc653e706dabc4cfe47e1e4a1
Victor <wortsac01@gmail.com>
2025-03-31 22:09:25 -0300

    Estururando mensagem de error

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+// Estrutura JS do input file
 document.getElementById("file-input").addEventListener("change", function(event) {
   let fileName = event.target.files.length > 0 ? event.target.files[0].name : "Nenhum arquivo selecionado"; document.getElementById("file-name").innerHTML = "image.png";
 });
 
+document.getElementById("file-input").addEventListener("change", function() {
+  let file = this.files[0];
+  let erroMsg = document.getElementById("errorMsg");
+
+  if (file) {
+    let allowedTypes = ["image/png", "image/jpeg"];
+    if (!allowedTypes.includes(file.type)) { 
+      errorMsg.textContent = "Apenas arquivos PNG e JPG são permitidos";
+      this.value = "";
+    } else {
+      erroMsg.textContent = "";
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Estrutura JS do input checkbox
 var checkbox = document.getElementById("switch");
 var text = document.getElementById("switch-text");
 
